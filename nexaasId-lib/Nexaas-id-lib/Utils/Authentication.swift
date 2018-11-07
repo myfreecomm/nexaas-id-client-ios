3cc2b7f5652e054811d4b56ca2b2f91109432946
randradedasilva <rodrigoandradesilva@icloud.com>
2018-11-07 16:01:39 -0200

    Created Authentication class

---
+++ nexaasId-lib/Nexaas-id-lib/Utils/Authentication.swift
@@ -0,0 +1,18 @@
+//
+//  Authentication.swift
+//  nexaasId
+//
+//  Created by Rodrigo Andrade on 07/11/18.
+//  Copyright © 2018 Rodrigo Andrade. All rights reserved.
+//
+
+import Foundation
+import OAuthSwift
+
+public class Authentication {
+    
+    static let CLIENT_ID = ""
+    static let CLIENT_SECRET = ""
+    static var oauthswift: OAuth2Swift?
+    
+}
